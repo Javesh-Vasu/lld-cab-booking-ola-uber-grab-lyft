2c104c32e201ee1343dadc2c3059309e60392a4d
Javesh Khungar <javeshkhungar@Javeshs-MacBook-Air.local>
2022-11-24 05:39:42 +0530

    added comment

--- src/main/java/com/uditagarwal/cabbooking/CabBookingTestApplication.java
+++ src/main/java/com/uditagarwal/cabbooking/CabBookingTestApplication.java
@@ -3,6 +3,7 @@ package com.uditagarwal.cabbooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//spring boot application
 @SpringBootApplication
 public class CabBookingTestApplication {
 
